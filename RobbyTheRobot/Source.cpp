436a570e52fc633fcda7cd7cc070df6c99602935
Aaron-T4 <Aarontroydunradletts@gmail.com>
2024-10-14 16:49:55 +0100

    week 4 task 4 left
    we did conditional statements today and were able to help robby find a path to the exit

--- RobbyTheRobot/Source.cpp
+++ RobbyTheRobot/Source.cpp
@@ -1,24 +1,45 @@
-#include <iostream>
-#include "GraphicsManager.h"	
-
-//Ignore this!!
-Room* room;
-Robot* robby;
-GraphicsManager* picture;
+//#include <iostream>
+//#include "GraphicsManager.h"	
 //
-
-int main()
-{
-	//declare the room, robby and picture objects here
-	room = new Room(0);
-	robby = new Robot();
-	picture = new GraphicsManager(room, robby);
-
-
-	robby->move();
-	picture->draw();
-
-
-	//we need system pause so that we can see where robby ends up
-	system("Pause");
-}
+////Ignore this!!
+//Room* room;
+//Robot* robby;
+//GraphicsManager* picture;
+////
+//
+//int main()
+//{
+//	//declare the room, robby and picture objects here
+//	room = new Room(5);
+//	robby = new Robot();
+//	picture = new GraphicsManager(room, robby);
+//
+//
+//
+//	robby->move();
+//	picture->draw();
+//
+//
+//	while (true)
+//	{
+//		if (robby->ahead_is_colour(room, "yellow"))
+//		{
+//			robby->move();
+//			picture->draw();
+//		}
+//		else if (robby->ahead_is_colour(room, "green")) {
+//			robby->move();
+//			picture->draw();
+//			break;
+//		}
+//		else 
+//		{
+//			robby->right();
+//		}
+//
+//	}
+//
+//
+//	//we need system pause so that we can see where robby ends up
+//	system("Pause");
+//}
